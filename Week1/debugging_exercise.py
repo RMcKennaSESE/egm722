0a006546e9d1c8c30c55c2274fa3febdf69ea8e6
RMcKennaSESE <rmckennasese@gmail.com>
2023-03-22 09:36:41 +0000

    Added guess count

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,7 +5,7 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
-
+guessNo = 1
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
@@ -14,5 +14,6 @@ while guess != rand:  # if the guess is not equal to the random number, you have
 
     print('Enter a new guess: ')
     guess = int(input())
-
+    guessNo = guessNo + 1
 print('You got it! The number was {}'.format(rand))
+print('Final number of guesses:', guessNo)
